7833065378a3bf0887d9a8619bfcb9f6cb1d33f5
Pranjal Gupta <pranjalgupta2199@gmail.com>
2018-06-01 00:30:10 +0530

    Improved search results

--- search.py
+++ search.py
@@ -1,27 +1,36 @@
 import sqlite3
 
 def Print_ (list_):
-	for i in list_:
-		print i
+	for i in range(len(list_)):
+		print i, list_[i]
 
 	if not list_ : 
 		print 'Sorry, No searches where found.'
 
 def course_code_search (query, cursor) : 
-	c = cursor
-	c.execute ( "SELECT COURSE_CODE FROM courses WHERE COURSE_CODE LIKE ? ", ('%' + query + '%',))
-	Print_ (c.fetchall())
 
-def course_title_search (query, cursor) :
-	c = cursor
-	c.execute( "SELECT COURSE_TITLE FROM courses WHERE COURSE_TITLE LIKE ?", ('%' + query + '%',))
-	Print_ (c.fetchall())
+	cursor.execute ( "SELECT COURSE_CODE FROM courses WHERE COURSE_CODE LIKE ? ", ('%' + query + '%',))
+	list_ = cursor.fetchall()
+	##After the user selects the desired result
+	##Only for command-line
+	Print_(list_)
+	
+	prompt = input(' match number :')	
+	result = list_[prompt]
 
-def instructor_search (query, cursor) :
-	c = cursor
-	c.execute( "SELECT DISTINCT INSTRUCTOR FROM courses WHERE INSTRUCTOR LIKE ?", ('%' + query + '%',))
-	Print_ (c.fetchall())
+	result_id = cursor.execute("SELECT _rowid_ FROM courses WHERE COURSE_CODE = ?", result).fetchall()[0]
 
+	cursor.execute("SELECT * FROM courses WHERE _rowid_ >= ? ", result_id )
+	List = [cursor.fetchone()]
+	try:
+		item = cursor.fetchone()
+		while (not item[0]):
+			List.append(item)
+			item = cursor.fetchone()		  
+	except: ##add excpetion type
+		pass
+	finally:
+		Print_(List)
 
 db = sqlite3.connect('courses.db')
 cursor = db.cursor()
@@ -30,6 +39,13 @@ while True:
 	print
 	print 
 	query = raw_input("Enter your query : ")
-	instructor_search(query, cursor)
+	course_code_search(query, cursor)
 
 
+def course_title_search (query, cursor) :
+	cursor.execute( "SELECT COURSE_TITLE FROM courses WHERE COURSE_TITLE LIKE ?", ('%' + query + '%',))
+	Print_ (cursor.fetchall())
+
+def instructor_search (query, cursor) :
+	a = cursor.execute( "SELECT DISTINCT INSTRUCTOR FROM courses WHERE INSTRUCTOR LIKE ?", ('%' + query + '%',))
+	Print_ (cursor.fetchall())
