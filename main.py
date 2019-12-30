7da25e93a680ef0d69f31e368919dfa625f44d7b
Pranjal Gupta <pranjalgupta2199@gmail.com>
2019-12-31 02:32:36 +0530

    bug: Fixes Multi-threading error in Gtk while pdf parsing

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from packages import page_00
 from gi.repository import Gtk
 
 if __name__ == "__main__" :
-    window0 = page_00.FileChooser()
-    try :
-        os.mkdir("Pages")
-        window0.show_all()
-        Gtk.main()
-    except :
-        pass
-    if window0.flag == 0 :
-        window1 = page_01.MyWindow()
-        window1.show_all()
-        Gtk.main()
+    # window0 = page_00.FileChooser()
+    # try :
+    #     os.mkdir("Pages")
+    #     window0.show_all()
+    #     Gtk.main()
+    # except :
+    #     pass
+    # if window0.flag == 0 :
+    window1 = page_01.MyWindow()
+    window1.show_all()
+    Gtk.main()
--- packages/syntax_check.py
+++ packages/syntax_check.py
@@ -10,15 +10,40 @@ db = sqlite3.connect('courses.db')
 db.create_function("REGEXP", 2, regexp)
 cursor = db.cursor()
 
-cursor.execute("SELECT _rowid_,INSTRUCTOR FROM courses WHERE INSTRUCTOR LIKE '%G206'")
-result = cursor.fetchall()
-print(result)
 
-for row in result:
-    row_id = row[0]
-    instructor = row[1][:-4]
-    room = row[1][-4:]
-    print((room, instructor, row_id))
-    cursor.execute('''UPDATE courses SET ROOM = ?, INSTRUCTOR = ? WHERE _rowid_ = ?''', (room,instructor,row_id))
+def rectify_courses(room_num):
+    cursor.execute("SELECT _rowid_,INSTRUCTOR,ROOM FROM courses WHERE INSTRUCTOR LIKE ?", ('%' + room_num + '%',))
+    result = cursor.fetchall()
+    print(result)
 
-db.commit()
+    for row in result:
+        row_id = row[0]
+        instructor = row[1][:-1*len(room_num)]
+        room = row[1][-1*len(room_num):] + row[2]
+        print((room, instructor, row_id))
+        cursor.execute('''UPDATE courses SET ROOM = ?, INSTRUCTOR = ? WHERE _rowid_ = ?''', (room,instructor,row_id))
+
+    db.commit()
+
+def rectify_midsems():
+    cursor.execute("SELECT _rowid_ FROM midsem WHERE TIME IS NULL OR DATES IS NULL")
+    result = cursor.fetchall()
+    print (result)
+
+    for row in result:
+        row_id = row[0]
+        cursor.execute("UPDATE midsem SET DATES = ?, TIME = ? WHERE _rowid_ = ?", ('*', '*', row_id))
+    
+    db.commit()
+    
+def main():
+    for block in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']:
+        for floor in ['0', '1', '2', '3']:
+            for room_num in range (0, 50):
+                room_num = block + floor + '0'*(2 - len(str(room_num)))+str(room_num)
+                # print (room_num)
+                rectify_courses(room_num)
+    rectify_midsems()
+
+if __name__ == "__main__":
+    main()
